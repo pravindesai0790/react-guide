f2ee482a92e88bc0488fca36c902cfd1545e6795
Pravin Desai <pravindesai0790@gmail.com>
2019-03-17 21:55:54 +0530

    Add css pseudo selectors feature using 'Radium'

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Radium from 'radium';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -53,7 +54,11 @@ class App extends Component {
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     };
 
     let persons = null;
@@ -75,6 +80,10 @@ class App extends Component {
       );
 
       style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      };
     }
 
     const classes = [];
@@ -97,5 +106,5 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Radium(App);
 
